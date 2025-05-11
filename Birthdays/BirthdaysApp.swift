5ce88d9fd91210f7d7df420f36ecbe7f52281de3
Ashley <anm38060@gmail.com>
2025-05-11 16:21:09 -0500

    Initial Commit

---
+++ Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by Scholar on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
